817e609cc5514ef82c2d051cd8daf87304270421
Panu Mantylahti <panu.mantylahti@gmail.com>
2023-04-03 20:15:02 +0300

    Ex 5/12 in progress.

--- exercises/Set4a.hs
+++ exercises/Set4a.hs
@@ -116,7 +116,13 @@ rangeOf xs = (-) y x
 --   longest [[1,2,3],[4,5],[6]] ==> [1,2,3]
 --   longest ["bcd","def","ab"] ==> "bcd"
 
-longest = todo
+-- longest = todo
+
+:{
+longest :: Ord a => [[a]] -> [Int]
+longest xs = 
+    map length xs
+:}
 
 ------------------------------------------------------------------------------
 -- Ex 6: Implement the function incrementKey, that takes a list of
